83086c861e00a3b2a81ce036196a3b70f39fe491
BomDia16 <arturbarbosaoliveira@mail.com>
2025-05-12 13:29:42 -0300

    Inplementação de getters e setters

--- src/Clientes.java
+++ src/Clientes.java
@@ -6,16 +6,53 @@ public class Clientes {
     private String nome;
     private double saldo;
 
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     // Inserir novos clientes
     public Clientes(int numero, String agencia, String nome, double saldo) {
-        this.numero = numero;
-        this.agencia = agencia;
-        this.nome = nome;
-        this.saldo = saldo;
+        setNumero(numero);
+        setAgencia(agencia);
+        setNome(nome);
+        setSaldo(saldo);
     }
 
     // Mensagem de listagem de clientes
     public String Mensagem() {
+        numero = getNumero();
+        nome = getNome();
+        agencia = getAgencia();
+        saldo = getSaldo();
+        
         return numero + ". " + nome + " | Agência: " + agencia + " | Saldo: R$ " + saldo;
     }
 }
